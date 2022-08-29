2cc72310687e364c0ccfb86cb0549852b97d89cc
Jeremiah Pagarigan <111500406+pjeremiah@users.noreply.github.com>
2022-08-29 12:17:12 +0800

    Create Weapon.java

---
+++ exercise_1/Weapon.java
@@ -0,0 +1,19 @@
+public class Weapon {
+
+    String Name;
+    int Damage;
+    int Weight;
+    String Rarity;
+    String weaponDescription;
+
+    public void sayName() {
+        System.out.println("Name of the Weapon: " + Name );
+    }
+    public void sayRarity() {
+        System.out.println("Rarity: " + Rarity );
+    }
+    public void sayweaponDescription() {
+        System.out.println("Description: " + weaponDescription );
+    }
+    
+}
