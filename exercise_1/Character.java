c3cbe9d4ebcf5723c3c719011326395b6ceca38d
Jeremiah Pagarigan <111500406+pjeremiah@users.noreply.github.com>
2022-08-29 12:57:11 +0800

    Create Character.java

---
+++ exercise_1/Character.java
@@ -0,0 +1,23 @@
+public class Character {
+    // Character attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    // Character method
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    public void sayMyStrength() {
+        System.out.println("My strength is " + strength);
+    }
+
+    public void sayMyAgility() {
+        System.out.println("My agility is " + agility);
+    }
+
+    public void sayMyIntelligence() {
+        System.out.println("My intelligence is " + intelligence);
+    }
+}
