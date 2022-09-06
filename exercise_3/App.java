346e54132e9f943238c65364ba84f92be1f480fd
Jeremiah Pagarigan <111500406+pjeremiah@users.noreply.github.com>
2022-09-06 15:19:58 +0800

    Update App.java

--- exercise_3/App.java
+++ exercise_3/App.java
@@ -3,29 +3,29 @@ public class App {
     public static void main(String[] args) {
   
       // Create new Stack Object
-      Stack cellphonebrand_stack = new Stack(5);
+      Stack Mask_stack = new Stack(5);
   
       // Insert new elements into the stack
-      cellphonebrand_stack.push("Oppo");
-      cellphonebrand_stack.push("Xiaomi Redmi");
-      cellphonebrand_stack.push("Apple");
-      cellphonebrand_stack.push("SamSung");
+      Mask_stack.push("Blue Mask");
+      Mask_stack.push("Green Mask");
+      Mask_stack.push("Yellow Mask");
+      Mask_stack.push("Black Mask");
   
       // Show the size of the stack
-      System.out.println("The size of the stack is: " + cellphonebrand_stack.size());
+      System.out.println("The size of the stack is: " + Mask_stack.size());
   
       // Show top element in the stack
-      System.out.println("The top of the stack is: " + cellphonebrand_stack.peek());
+      System.out.println("The top of the stack is: " + Mask_stack.peek());
   
       // Remove top element in the stack
-      cellphonebrand_stack.pop();
+      Mask_stack.pop();
   
       System.out.println("After pop");
   
       // Show all elements in Stack
-      cellphonebrand_stack.printStack();
+      Mask_stack.printStack();
   
       // Show top element in the stack
-      System.out.println("The top of the stack is: " + cellphonebrand_stack.peek());
+      System.out.println("The top of the stack is: " + Mask_stack.peek());
     }
   }
